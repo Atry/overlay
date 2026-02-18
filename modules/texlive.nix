231a7f0429edff1d3e53736e48b3245e3205a9e5
Yang, Bo <yang-bo@yang-bo.com>
2026-02-18 09:16:45 +0000

    refactor: streamline package-arxiv script integration in texlive.nix

--- modules/texlive.nix
+++ modules/texlive.nix
@@ -1,49 +1,44 @@
 { ... }: {
   imports = [ ./dev.nix ];
-  partitions.dev.module.perSystem = { pkgs, ... }:
-    let
-      package-arxiv = pkgs.writeShellApplication {
-        name = "package-arxiv";
-        runtimeInputs = [ pkgs.gnutar ];
-        text = ''
-          tar -czvf arxiv-submission.tar.gz \
-            -C overlay-calculus \
-            *.tex \
-            *.bib \
-            *.bst \
-            *.cls
-        '';
-      };
-    in {
-      ml-ops.devcontainer.devenvShellModule = {
-        packages = [ pkgs.tex-fmt package-arxiv ];
-        languages = {
-          texlive = {
-            enable = true;
-            packages = [
-              "scheme-medium"
-              "cjk"
-              "xpinyin"
-              "latexmk"
-              # acmart dependencies not in scheme-medium
-              "xstring"
-              "totpages"
-              "environ"
-              "trimspaces"
-              "ncctools"
-              "comment"
-              "pbalance"
-              "libertine"
-              "inconsolata"
-              "newtx"
-              "hyperxmp"
-              "ifmtarg"
-              "draftwatermark"
-              "preprint"
-              "tex-gyre"
-            ];
-          };
+  partitions.dev.module.perSystem = { pkgs, lib, ... }: {
+    ml-ops.devcontainer.devenvShellModule = {
+      packages = [ pkgs.tex-fmt ];
+      scripts.package-arxiv.exec = ''
+        cd overlay-calculus
+        ${lib.getExe pkgs.gnutar} -czvf arxiv-submission.tar.gz \
+          -C . \
+          *.tex \
+          *.bib \
+          *.bst \
+          *.cls
+      '';
+      languages = {
+        texlive = {
+          enable = true;
+          packages = [
+            "scheme-medium"
+            "cjk"
+            "xpinyin"
+            "latexmk"
+            # acmart dependencies not in scheme-medium
+            "xstring"
+            "totpages"
+            "environ"
+            "trimspaces"
+            "ncctools"
+            "comment"
+            "pbalance"
+            "libertine"
+            "inconsolata"
+            "newtx"
+            "hyperxmp"
+            "ifmtarg"
+            "draftwatermark"
+            "preprint"
+            "tex-gyre"
+          ];
         };
       };
     };
+  };
 }
